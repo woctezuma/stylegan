eaf1f9fb34da651b4c926cd10af2583408191a2f
Wok <woctezuma@users.noreply.github.com>
2019-02-22 21:59:37 +0100

    Fix typo

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ if 1:
     #desc += '-bedroom';  dataset = EasyDict(tfrecord_dir='lsun-bedroom-full');    train.mirror_augment = False
     #desc += '-car';      dataset = EasyDict(tfrecord_dir='lsun-car-512x384');     train.mirror_augment = False
     #desc += '-cat';      dataset = EasyDict(tfrecord_dir='lsun-cat-full');        train.mirror_augment = False
-    desc += '-steam';      dataset = EasyDict(tfrecord_dir='128x128');        train.mirror_augment = False
+    desc += '-steam';      dataset = EasyDict(tfrecord_dir='stylegan');        train.mirror_augment = False
 
     # Number of GPUs.
     desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}
